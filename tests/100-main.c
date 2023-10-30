ee178b9d896a295412f55c9ab02887e440acd0df
Khalifa Bako <123035663+Khalifabako14@users.noreply.github.com>
2023-10-30 16:28:34 +0000

    Binary Trees

--- tests/100-main.c
+++ tests/100-main.c
@@ -3,11 +3,12 @@
 #include "../binary_trees.h"
 
 /**
- * launch_test - Test ancestor function and print informations
- *
+ * launch_test - A function that tests the 
+ * ancestor function and print informations
  * @n1: First node
  * @n2: Second node
  */
+
 void launch_test(binary_tree_t *n1, binary_tree_t *n2)
 {
     binary_tree_t *ancestor;
