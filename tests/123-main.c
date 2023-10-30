9f8c0055aa89cffd8291ea8d8dbeb6f879376a77
Khalifa Bako <123035663+Khalifabako14@users.noreply.github.com>
2023-10-30 16:45:11 +0000

    Binary Trees

--- tests/123-main.c
+++ tests/123-main.c
@@ -3,10 +3,10 @@
 #include "../binary_trees.h"
 
 /**
- * main - Entry point
- *
+ * main - The entry point
  * Return: 0 on success, error code on failure
  */
+
 int main(void)
 {
     avl_t *tree;
